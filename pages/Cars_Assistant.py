76c31da02b72f985f1a57f9d1a9caa742afd9325
Mikey-nk <kibe50067@gmail.com>
2025-07-22 14:34:32 +0300

    car mod2

--- pages/Cars_Assistant.py
+++ pages/Cars_Assistant.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import google.generativeai as genai
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 # Load API key from environment variable
 api_key = os.getenv("GEMINI_API_KEY")
 
